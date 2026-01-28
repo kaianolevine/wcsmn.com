407cfd121dea89687615eca66b3d6500385e4fbb
Kaiano Levine <kaiano.levine@gmail.com.com>
2026-01-28 11:46:25 -0600

    feat: bringing the wensite structure together

--- src/_data/navigation.js
+++ src/_data/navigation.js
@@ -1,21 +1,51 @@
 export default {
   top: [
     {
-      text: 'About',
-      url: '/about/'
+      text: 'WCS MN',
+      url: '/index.html'
     },
     {
-      text: 'Docs',
-      url: '/get-started/'
+      text: 'Template',
+      url: '/template/'
     },
     {
-      text: 'Built with',
-      url: '/built-with/'
+      text: 'Community Resources',
+      url: '/community-resources/'
     },
     {
-      text: 'Blog',
-      url: '/blog/'
-    }
+      text: 'Organizations',
+      url: '/organizations/',
+      submenu: [
+        {
+          text: 'MN Swing Safety',
+          url: '/organizations/mn-swing-safety/'
+        },
+        {
+          text: 'West Cone Swing',
+          url: '/organizations/west-cone-swing/'
+        },
+        {
+          text: 'TC Rebels',
+          url: '/organizations/tc-rebels/'
+        },
+        {
+          text: 'Swing in the North',
+          url: '/organizations/swing-in-the-north/'
+        },
+        {
+          text: 'SWINGesota',
+          url: '/organizations/swingesota/'
+        },
+        {
+          text: 'MN WCS Dance Club',
+          url: '/organizations/mn-wcs-dance-club/'
+        }
+      ]
+    },
+    {
+      text: 'Contact Us',
+      url: '/contact-us/'
+    },
   ],
   bottom: [
     {
